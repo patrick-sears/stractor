a9ab683b14dec6eb9966de1bd7eb603649fcbe5a
Patrick R Sears <pat@pskate.OS3>
2018-06-08 07:38:00 -0400

    Removed aas and stractor_d sytems.  Added gitignore.

--- install.sh
+++ install.sh
@@ -10,13 +10,9 @@ if [[ -e "prs-stractor" ]]; then
   rm "prs-stractor"
 fi
 
-if [[ -e "prs-stractor_d" ]]; then
-  rm "prs-stractor_d"
-fi
 
 
-ln -sf "$wd/aas" "prs-stractor"
-ln -sf "$wd" "prs-stractor_d"
+ln -sf "$wd/stractor.py" "prs-stractor"
 
 
 
